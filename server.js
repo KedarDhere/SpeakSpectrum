df75a689d3c20da886e6df47868c3cdba353eafd
KedarDhere <dherek@oregonstate.edu>
2023-07-31 20:21:01 -0700

    Defined port variable

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const SpeechSDK = require('microsoft-cognitiveservices-speech-sdk');
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist/speak-spectrum'));
 app.use(cors());
